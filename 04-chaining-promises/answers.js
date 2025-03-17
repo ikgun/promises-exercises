cb487342bf571659861faf89acfd1133bd7c1c4a
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-17 12:37:21 +0100

    Implement flatMapPromise method

--- 04-chaining-promises/answers.js
+++ 04-chaining-promises/answers.js
@@ -1,37 +1,44 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {Promise} promise 
- * @param {function} asyncTransformer 
+ *
+ * @param {Promise} promise
+ * @param {function} asyncTransformer
  */
-function flatMapPromise(promise, asyncTransformer){
+function flatMapPromise(promise, asyncTransformer) {
   return new Promise((resolve, reject) => {
     promise
-      .then(/* IMPLEMENT ME! */);
+      .then((val) => {
+        try {
+          resolve(asyncTransformer(val));
+        } catch (error) {
+          reject(error);
+        }
+      })
+      .catch((err) => reject(err));
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} firstPromise 
- * @param {function} slowAsyncProcess 
+ *
+ * @param {Promise} firstPromise
+ * @param {function} slowAsyncProcess
  */
-function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess) {
   return firstPromise.then(/* IMPLEMENT ME! */);
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
- * @param {function} getUserById 
- * @param {function} getOrganizationById 
+ *
+ * @param {function} getUserById
+ * @param {function} getOrganizationById
  */
-function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
-  return function getUserByIdWithOrganization(userId){
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById) {
+  return function getUserByIdWithOrganization(userId) {
     /* IMPLEMENT ME! */
   };
 }
